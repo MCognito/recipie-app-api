17b1f68151c391462143d9a301f2a1d1c0d3a6b6
Mandeep <mandeepnpandya@gmail.com>
2025-08-13 17:03:41 +0100

    Configured Docker Compose and checks to wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,18 +6,19 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
     def test_add(self):
         """Test adding numbers"""
         res = calc.add(10, 5)
         self.assertEqual(res, 15)
-        
+
     def test_subtract(self):
         """Test subtracting numbers"""
         res = calc.subtract(10, 5)
         self.assertEqual(res, 5)
-    
+
     def test_multiply(self):
         """Test multiplying numbers"""
         res = calc.multiply(10, 5)
@@ -27,5 +28,3 @@ class CalcTests(SimpleTestCase):
         """Test dividing numbers"""
         res = calc.divide(10, 5)
         self.assertEqual(res, 2)
-
-        
