4ce1437d44ec4afd505731075e283d9dc87f31b3
Mandeepsource <mandeepnpandya@gmail.com>
2025-08-14 20:23:55 +0100

    Fixed Linter Warnings

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,7 +35,7 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
-    
+
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
@@ -43,6 +43,9 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a super user"""
-        user = get_user_model().objects.create_superuser(email='test@example.com', password='password123')
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com',
+            password='password123'
+        )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
